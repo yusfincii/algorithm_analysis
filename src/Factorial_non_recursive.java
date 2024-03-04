0267b19c0b4f0a8ba9e8ec1d0ea1fbb2993ff4bd
yusuf <ysfnc27@gmail.com>
2024-03-04 11:26:47 +0300

    init

---
+++ src/Factorial_non_recursive.java
@@ -0,0 +1,17 @@
+public class Factorial_non_recursive {
+
+    // returns the factorial of a given number with loop
+    public static int factorial(int num){
+        int result = 1;
+        for(int i=num; i>0; i--){
+            // main operator
+            result *= i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(0));
+        System.out.println(factorial(10));
+    }
+}
