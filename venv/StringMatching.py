908f5872cdd9abec9c6b452b3fb9959f6d90857a
yusuf <ysfnc27@gmail.com>
2024-03-15 11:17:50 +0300

    new

---
+++ venv/StringMatching.py
@@ -0,0 +1,18 @@
+def string_matching(text, pattern):
+    for i in range(0, len(text) - len(pattern)):
+        j = 0
+        # condition of while represent the character matching situation
+        # so loop will work when text's corresponding char match with pattern's char
+        while j < len(pattern) and pattern[j] == text[j + i]:
+            j += 1
+
+        if j == len(pattern):
+            return i
+    # not found situation
+    return -1
+
+
+# for testing
+text = "Lorem ipsum dolar amet"
+pattern = "ola"
+print(string_matching(text, pattern))
