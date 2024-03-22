d29bfb2351b390d75c38001179ada5c3f4521b9c
yusuf <ysfnc27@gmail.com>
2024-03-22 19:03:37 +0300

    swap func and commlinex added

---
+++ src/QuickSort.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class QuickSort {
+
+    // a util swap function
+    public static void swap(int[] arr, int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+
+    public static void partition(int[] arr, int left, int right){
+
+    }
+
+    public static int[] quickSort(int[] arr, int left, int right){
+        return arr;
+
+        // Time Complexity :
+    }
+
+    public static void main(String[] args) {
+        /*// test for swap function
+        int[] arrx = {1, 3, 5};
+        swap(arrx, 0, 2);
+        System.out.println(Arrays.toString(arrx));*/
+
+
+    }
+}
